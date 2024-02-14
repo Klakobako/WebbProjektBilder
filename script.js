59785df4c2ea8496155a3fbe67b76a70f038028a
Axel Eriksson <Axel.Eriksson@student.tabyenskilda.se>
2024-02-14 12:35:05 +0100

    js

---
+++ script.js
@@ -0,0 +1,7 @@
+function byttillbild1() {
+    document.getElementById('berg').src = 'marek-piwnicki-italianmountains-unsplash.jpg';
+}
+function byttilllogga() {
+    document.getElementById('berg').src = 'sam-carter-sheep-unsplash.jpg';
+    }
+    
